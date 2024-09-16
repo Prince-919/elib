690c14be95b659f56f50e9aaab5c2ab04544da32
Prince-919 <princesharmabbu0@gmail.com>
2024-09-16 23:30:38 +0530

    add user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Response, Request } from "express";
+import createHttpError from "http-errors";
 
 export const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
   res.json({ message: "User created" });
 };
