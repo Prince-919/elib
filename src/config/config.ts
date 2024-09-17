d13a7ae081425e9bdf9dc57f63099c51988d011a
Prince-919 <princesharmabbu0@gmail.com>
2024-09-17 10:03:56 +0530

    add error handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const _config = {
-    port : process.env.PORT,
-    databaseUrl:process.env.MONGO_CONNECTION_STRING,
-    env:process.env.NODE_ENV
-}
+  port: process.env.PORT,
+  databaseUrl: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
+};
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Response, Request } from "express";
 import createHttpError from "http-errors";
 import User from "./userModel";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+import { User as UserType } from "./userTypes";
 
 export const createUser = async (
   req: Request,
@@ -14,13 +17,37 @@ export const createUser = async (
     return next(error);
   }
 
-  const user = await User.findOne();
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email");
-    return next(error);
+  try {
+    const user = await User.findOne();
+    if (user) {
+      const error = createHttpError(400, "User already exists with this email");
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, "Error while getting user"));
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  res.json({ message: "User created" });
+  let newUser: UserType;
+  try {
+    newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return next(createHttpError(500, "Error while creating user"));
+  }
+
+  try {
+    const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "Error while signing the jwt token"));
+  }
 };
