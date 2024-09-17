0140a652c2f04d15ae19292f88c044ad2797b0f1
Prince-919 <princesharmabbu0@gmail.com>
2024-09-17 13:41:42 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,50 @@
+import path from "node:path";
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import createHttpError from "http-errors";
 
 export const createBook = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const files = req.files as { [filename: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  const bookFileName = files.file[0].filename;
+  const bookfilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    bookFileName
+  );
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "books-cover",
+      format: coverImageMimeType,
+    });
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookfilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return next(createHttpError(500, "Error while uploading the files"));
+  }
+
+  res.json({});
+};
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,21 @@
+import path from "node:path";
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
 
 const router = express.Router();
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, // 3e7 => 35mb
+});
 
-router.post("/", createBook);
+router.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default router;
